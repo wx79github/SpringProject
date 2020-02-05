e3751f30dd0b79694073843bc9a44d6ff00e18a6
wx79github <wangxin_9979@163.com>
2020-02-06 00:09:17 +0800

    提交代码

--- src/com/spring/beans/autowire/Main.java
+++ src/com/spring/beans/autowire/Main.java
@@ -16,9 +16,10 @@ public class Main {
 //        Person person = new Person();
 //        person =(Person) ctx.getBean("person");
 //        System.out.println(person);
-        //自动装配
+        //自动装配1
         Person person1 = new Person();
         person1 =(Person) ctx.getBean("person1");
         System.out.println(person1);
+        System.out.println(person1.getCar());
     }
 }
